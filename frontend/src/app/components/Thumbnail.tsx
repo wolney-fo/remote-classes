a02b5e9810c948a80dda4817c6fe7e04e7e50bc1
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-07-22 23:11:34 -0300

    feat: update no current class situation

--- frontend/src/app/components/Thumbnail.tsx
+++ frontend/src/app/components/Thumbnail.tsx
@@ -1,14 +1,14 @@
 interface ThumbnailProps {
-  thumbnailUrl: string;
-  subject: string;
+  thumbnailUrl?: string;
+  subject?: string;
 }
 
 export default function Thumbnail(props: ThumbnailProps) {
   return (
     <figure className="relative overflow-hidden before:absolute before:content-[''] before:inset-0 before:top-[50%] before:bg-thumb-gradient">
       <img
-        src={props.thumbnailUrl}
-        alt={props.subject}
+        src={props.thumbnailUrl ?? "/waiting.jpg"}
+        alt={props.subject ?? "No class now"}
         width={900}
         height={367}
         draggable="false"
@@ -16,8 +16,8 @@ export default function Thumbnail(props: ThumbnailProps) {
       />
 
       <figcaption className="absolute bottom-0 p-6 text-white">
-        <span className="text-xl">Live now:</span>
-        <h2 className="text-6xl">{props.subject}</h2>
+        {props.subject && <span className="text-xl">Live now:</span>}
+        <h2 className="text-6xl">{props.subject ?? "Nothing happening now"}</h2>
       </figcaption>
     </figure>
   );
